69a54327512d1353352209670be2c38537348588
Ayush <ayushyadav102030tt@gmail.com>
2026-07-16 14:57:45 +0530

    Added login feature

--- appointment.py
+++ appointment.py
@@ -1,2 +1,7 @@
 print("Hospital Appointment Booking System")
 print("Patient Registration Module Added")
+
+print("Login Feature")
+username = input("Enter Username: ")
+password = input("Enter Password: ")
+print("Login Successful")
